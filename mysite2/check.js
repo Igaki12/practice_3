2a32fe45a554cbe23833baece25db4920b7874e0
Igaki12 <91039412+Igaki12@users.noreply.github.com>
2021-10-16 14:22:54 +0900

    Add files via upload
    お問い合わせのjs周りを修正しました

--- mysite2/check.js
+++ mysite2/check.js
@@ -2,12 +2,15 @@
   function check() {
     if (contact.user_name.value == "") {
       alert("名前を入力してください");
+      return false;
     }
     if(contact.gender.value == "") {
-      alert("性別を選んでください")
+      alert("性別を選んでください");
+      return false;
     }
     if (contact.form_box.value == "ここに記入してください" || contact.form_box.value == ""){
-      alert("問い合わせ内容を入力してください")
+      alert("問い合わせ内容を入力してください");
+      return false;
     }
 
   }
